77309e9ce4fa03e9612ae155c88831cdddc598db
Una <Una21136@yahoo.com>
2013-02-28 13:49:48 -0500

    The original files for the app. Coded in HTML/CSS. Home screen still needs resolving

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+ 
+ var viewportwidth;
+ var viewportheight;
+ 
+ // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+ 
+ if (typeof window.innerWidth != 'undefined')
+ {
+      viewportwidth = window.innerWidth,
+      viewportheight = window.innerHeight
+ }
+ 
+// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+ 
+ else if (typeof document.documentElement != 'undefined'
+     && typeof document.documentElement.clientWidth !=
+     'undefined' && document.documentElement.clientWidth != 0)
+ {
+       viewportwidth = document.documentElement.clientWidth,
+       viewportheight = document.documentElement.clientHeight
+ }
+
+function deviceheight(){
+	return(viewportheight + "px");
+	};
+
+	deviceheight();
